346c0921fd400186c7f6a29e255bb36fc3fc631d
Yuta Seki <sekiyubusi@gmail.com>
2026-08-19 22:18:29 +0900

    Switch capacitor.config.ts to capacitor.config.json to avoid requiring TypeScript in CI

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.yutaXXX.highlowrush',
-  appName: 'ハイローラッシュ',
-  webDir: 'www'
-};
-
-export default config;
